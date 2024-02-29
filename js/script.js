a825e4342969d04ef1d37ad8b13d344e3aaf63a2
Alpakah <hahnsuk@gmail.com>
2024-02-28 19:44:42 -0800

    Fixed Home Page Image cropping

--- js/script.js
+++ js/script.js
@@ -18,11 +18,11 @@ function changeNavbar(){
 }
 
 // Make Home page image scroll slower then the rest of the page
-document.getElementById("body").onscroll = function myFunction() {  
+document.body.onscroll = function slowScroll() {  
     var scrolltotop = document.scrollingElement.scrollTop;
     var target = document.getElementById("home-page");
     var xvalue = "center";
-    var factor = 0.5;
+    var factor = 0.7;
     var yvalue = scrolltotop * factor;
     target.style.backgroundPosition = xvalue + " " + yvalue + "px";
 }
