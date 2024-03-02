8112804455872bacc1495dcd3826f357359ef5a0
Alpakah <hahnsuk@gmail.com>
2024-03-02 00:03:13 -0800

    General framework of website complete?
    Initial framework of website seems to be complete -- need to replace content with actual final content, but the structure seems to be finalized (fingers crossed)

--- js/script.js
+++ js/script.js
@@ -2,12 +2,16 @@
     Javascript Sheet
 */
 
-// Make Home page image scroll slower then the rest of the page
+// Make all title pages image scroll slower then the rest of the page
+
 document.body.onscroll = function slowScroll() {  
     var scrolltotop = document.scrollingElement.scrollTop;
-    var target = document.getElementById("home-page");
+    var target = document.getElementsByClassName("header-block");
     var xvalue = "center";
     var factor = 0.7;
     var yvalue = scrolltotop * factor;
-    target.style.backgroundPosition = xvalue + " " + yvalue + "px";
+
+    for (var i = 0; i < target.length; i++) {
+        target[i].style.backgroundPosition = xvalue + " " + yvalue + "px";
+    }
 }
